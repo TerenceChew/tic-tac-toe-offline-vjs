cc66a5c5df1f382e63d8d41d4d1125b0a81209c7
TerenceChew <wschew97@gmail.com>
2022-06-27 22:06:24 +0800

    refactored code

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,12 @@ let gameEnd = false;
 let currentPlayer = 'X';
 let winner = '';
 let boardArr = new Array(9).fill('');
- 
+
+// add event listener to each board position
+boardPositions.forEach(position => {
+  position.addEventListener('click', handleBoardClick)
+})
+
 // handle board click
 function handleBoardClick(event) {
   let { id, classList } = event.target;
@@ -18,7 +23,7 @@ function handleBoardClick(event) {
     boardArr[id] = currentPlayer;
     currentPlayer === 'X' ? classList.add('purple') : classList.add('cyan');
     updateBoardPositions();
-    checkWon(); // always check for win whenever board updates
+    checkWin(); // always check for win whenever board updates
   }
 }
 
@@ -29,30 +34,13 @@ function updateBoardPositions() {
   });
 }
 
-// add event listener to each board position
-boardPositions.forEach(position => {
-  position.addEventListener('click', handleBoardClick)
-})
-
-// show the winner
-function showResult(winner) {
-  let result = document.createElement('p');
-  result.innerText = winner === 'DRAW!' ? winner : `${winner} Won!`;
-  result.classList.add('result', 'animate__animated', 'animate__flash', 'animate__infinite');
-  page.append(result);
-}
-
-// show the new game button
-function showButton() {
-  let btn = document.createElement('button');
-  btn.innerText = 'New Game';
-  btn.classList.add('btn', 'animate__animated', 'animate__zoomIn');
-  btn.addEventListener('click', handleButtonClick); // add event listener to button
-  page.append(btn);
-}
-
 // handle button click to reset the game
 function handleButtonClick() {
+  resetGame();
+}
+
+// reset game
+function resetGame() {
   removeAnimation();
   turn.innerText = 'X\'s Turn';
   gameEnd = false;
@@ -83,14 +71,39 @@ function addAnimation() {
 }
 
 // end the game
-function endGame() {
+function endGame(winner) {
+  showResult(winner);
+  showButton();
   gameEnd = true;
   turn.innerText = 'GAME END';
 }
 
+// show the winner
+function showResult(winner) {
+  let result = document.createElement('p');
+  result.innerText = winner === 'DRAW!' ? winner : `${winner} Won!`;
+  result.classList.add('result', 'animate__animated', 'animate__flash', 'animate__infinite');
+  page.append(result);
+}
+
+// show the new game button
+function showButton() {
+  let btn = document.createElement('button');
+  btn.innerText = 'New Game';
+  btn.classList.add('btn', 'animate__animated', 'animate__zoomIn');
+  btn.addEventListener('click', handleButtonClick); // add event listener to button
+  page.append(btn);
+}
+
+// switch to next player
+function switchPlayer() {
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X'; // change player
+  turn.innerText = `${currentPlayer}'s Turn`;
+}
+
 // check for win
-function checkWon() {
-  const winningConditions = [
+function checkWin() {
+  const winningCombinations = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -100,33 +113,27 @@ function checkWon() {
     [0,4,8],
     [2,4,6]
   ];
-
-  let winnerFound = winningConditions.some(condition => {
-    let a = boardArr[condition[0]];
-    let b = boardArr[condition[1]];
-    let c = boardArr[condition[2]];
+  // check if any combination is true
+  let winnerFound = winningCombinations.some(combination => {
+    let a = boardArr[combination[0]];
+    let b = boardArr[combination[1]];
+    let c = boardArr[combination[2]];
     
     return a !== '' && a === b && b === c;
   });
 
+  // check for any empty positions on board
+  let boardIsNotFull = boardArr.includes('');
+
   if (winnerFound) {
     winner = currentPlayer;
-    showResult(winner);
-    showButton();
-    endGame();
+    endGame(winner);
+  } else if (boardIsNotFull) {
+    switchPlayer();
   } else {
-    let boardIsNotFull = boardArr.includes('');
-
-    if (boardIsNotFull) {
-      currentPlayer = currentPlayer === 'X' ? 'O' : 'X'; // change player
-      turn.innerText = `${currentPlayer}'s Turn`;
-    } else {
-      winner = 'DRAW!';
-      showResult(winner);
-      showButton();
-      endGame();
-    };
-  };
+    winner = 'DRAW!';
+    endGame(winner);
+  }
 }
 
 
